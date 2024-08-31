3a227d4fd89d87789343f138544052c07a998516
Beksultan A <babdikaarov@gmail.com>
2024-09-01 02:41:54 +0300

    feat(nx): root

--- src/renderers/BaseCollage.js
+++
@@ -1,7 +0,0 @@
-// BaseCollage.js
-
-export class BaseCollage {
-   render() {
-      console.log("Rendering base renderer");
-   }
-}
--- test/test.js
+++
@@ -1,7 +0,0 @@
-import { Collagio } from "../src/core/Collagio";
-import { GridCollage } from "/src/renderers/GridCollage.js";
-import { MasonryCollage } from "/src/renderers/MasonryCollage.js";
-
-const test = new Collagio()
-
-test.
